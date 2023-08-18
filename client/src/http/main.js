d595ea8ac3f58559107db1c92dde2b8dc0a8fa1d
Leon <6860571@stud.nau.edu.ua>
2023-08-18 16:47:26 +0300

    try to cloud

--- client/src/http/main.js
+++ client/src/http/main.js
@@ -10,7 +10,8 @@ if (process.env.REACT_APP_MODE === 'production') {
   $host = axios.create({
     baseURL: process.env.REACT_APP_API_CLIENT_URL,
   })
+} else {
+  $host = process.env.REACT_APP_API_SERVER_URL
 }
 
 export { $host }
-// development
